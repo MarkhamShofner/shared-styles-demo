fea0e47ef247d2cf67a1ca98c67283661802a7cf
Markham Shofner <markham.shofner@gmail.com>
2016-06-02 16:58:34 -0400

    adjust formatting in script file

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'shiftyshofner.okh88o37',
-    accessToken: 'pk.eyJ1Ijoic2hpZnR5c2hvZm5lciIsImEiOiJjaW95amQ2eGowMXZudWZtNGh1dnZ0Z3F3In0.hsRaBxa8sODVT3Sl50E6cQ'
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'shiftyshofner.okh88o37',
+  accessToken: 'pk.eyJ1Ijoic2hpZnR5c2hvZm5lciIsImEiOiJjaW95amQ2eGowMXZudWZtNGh1dnZ0Z3F3In0.hsRaBxa8sODVT3Sl50E6cQ'
 }).addTo(mymap);
 
 var lineData = [{
@@ -28,33 +28,33 @@ var lineData = [{
 }];
 
 var vis = d3.select('#visualisation'),
-    WIDTH = 1000,
-    HEIGHT = 500,
-    MARGINS = {
-      top: 20,
-      right: 20,
-      bottom: 20,
-      left: 50
-    },
-    xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(lineData, function(d) {
-      return d.x;
-    }), d3.max(lineData, function(d) {
-      return d.x;
-    })]),
-    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function(d) {
-      return d.y;
-    }), d3.max(lineData, function(d) {
-      return d.y;
-    })]),
-    xAxis = d3.svg.axis()
-      .scale(xRange)
-      .tickSize(5)
-      .tickSubdivide(true),
-    yAxis = d3.svg.axis()
-      .scale(yRange)
-      .tickSize(5)
-      .orient('left')
-      .tickSubdivide(true);
+  WIDTH = 1000,
+  HEIGHT = 500,
+  MARGINS = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 50
+  },
+  xRange = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(lineData, function(d) {
+    return d.x;
+  }), d3.max(lineData, function(d) {
+    return d.x;
+  })]),
+  yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function(d) {
+    return d.y;
+  }), d3.max(lineData, function(d) {
+    return d.y;
+  })]),
+  xAxis = d3.svg.axis()
+  .scale(xRange)
+  .tickSize(5)
+  .tickSubdivide(true),
+  yAxis = d3.svg.axis()
+  .scale(yRange)
+  .tickSize(5)
+  .orient('left')
+  .tickSubdivide(true);
 
 vis.append('svg:g')
   .attr('class', 'x axis')
@@ -66,7 +66,7 @@ vis.append('svg:g')
   .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
   .call(yAxis);
 
-  var lineFunc = d3.svg.line()
+var lineFunc = d3.svg.line()
   .x(function(d) {
     return xRange(d.x);
   })
@@ -75,7 +75,7 @@ vis.append('svg:g')
   })
   .interpolate('linear');
 
-  vis.append('svg:path')
+vis.append('svg:path')
   .attr('d', lineFunc(lineData))
   .attr('stroke', 'blue')
   .attr('stroke-width', 2)
